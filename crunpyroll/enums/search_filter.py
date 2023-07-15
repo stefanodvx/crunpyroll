a3afa9d27b2c5b0f087470ae6ec8bc98bb3ebcac
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2023-07-15 13:46:10 +0200

    Update search_filter.py

---
+++ crunpyroll/enums/search_filter.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class SearchFilter(Enum):
+    MUSIC = "music"
+    SERIES = "series"
+    EPISODE = "episode"
+    TOP_RESULTS = "top_results"
+    MOVIE_LISTING = "movie_listing"
