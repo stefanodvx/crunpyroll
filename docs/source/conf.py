6a397e46d49fe96e25aad46ba623323733e9d8a5
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2024-01-07 19:29:01 +0100

    2.2.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
 
-project = 'Crunpyroll Documentation'
+project = 'Crunpyroll'
 copyright = '2024, stefanodvx'
 author = 'stefanodvx'
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,6 +1,3 @@
-Welcome to Crunpyroll
-=====================
-
 .. raw:: html
 
     <div>
