afa168b7644b5739013544b2385ac5302cf623c8
ethanbergstrom <ethan.bergstrom@gmail.com>
2026-07-25 13:29:56 -0500

    Update token

--- crunpyroll/utils.py
+++ crunpyroll/utils.py
@@ -2,7 +2,7 @@ from typing import Optional, List, Dict
 from datetime import datetime
 from uuid import uuid4
 
-PUBLIC_TOKEN = "dC1rZGdwMmg4YzNqdWI4Zm4wZnE6eWZMRGZNZnJZdktYaDRKWFMxTEVJMmNDcXUxdjVXYW4="
+PUBLIC_TOKEN = "b2VkYXJteHN0bGgxanZhd2ltbnE6OWxFaHZIWkpEMzJqdVY1ZFc5Vk9TNTdkb3BkSnBnbzE="
 
 APP_VERSION = "3.59.0"
 
